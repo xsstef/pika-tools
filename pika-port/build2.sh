a74b8c03e608cffd229639642439aa4428833e3a
Buzzzzzzyear <34020312+xsstef@users.noreply.github.com>
2020-07-10 17:17:25 +0800

    Update build2.sh

--- pika-port/build2.sh
+++ pika-port/build2.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
-# ******************************************************
-# DESC    : pika-port2 build script
-# AUTHOR  : Alex Stocks
-# VERSION : 1.0
-# LICENCE : Apache License 2.0
-# EMAIL   : alexstocks@foxmail.com
-# MOD     : 2019-01-22 19:54
-# FILE    : build.sh
-# ******************************************************
+PORT_PATH=~/pika-tools
+#1. install the third libs
+git submodule update --init --recursive
 
-# https://github.com/facebook/rocksdb/blob/master/INSTALL.md
+#2. install compression lib
+yum install -y snappy snappy-devel
+yum install -y zlib zlib-devel
+yum install -y bzip2 bzip2-devel
+yum install -y lz4-devel
 
-sh glog.sh
+#3. install gflags
+if [ ! -d "$(PORT_PATH)/third/gflags" ]; then
+  git clone https://github.com/gflags/gflags.git
+  cd gflags && git chekout v2.0 
+fi
+./configure --prefix=/usr/local
+make && make install
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
-# install compression lib
-sudo yum install -y snappy snappy-devel
-sudo yum install -y zlib zlib-devel
-sudo yum install -y bzip2 bzip2-devel
-sudo yum install -y lz4-devel
+#4. install glog
+cd $(PORT_PATH)/third/glog
+./configure --prefix=/usr/local
+make && make install
 
-# download third libs
-# pls wait for about 30 minutes
-git submodule update --init --recursive --force
-
-# compile pika-port for pika 3.0.x
-cd pika_port_2 && make
+#5. compile pika 2.3.7
+cd $PORT_PATH/pika-port/pika_port_2/
+make
